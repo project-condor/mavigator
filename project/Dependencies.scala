0355bb69a4942082d598b5c08fd4b071e931ee87
Jakob Odersky <jodersky@gmail.com>
2015-01-20 16:46:08 +0100

    update dependency to flow

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ object Dependencies {
 
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.3.6"
 
-  val flow = "com.github.jodersky" %% "flow" % "2.0.8"
-  val flowNative = "com.github.jodersky" % "flow-native" % "2.0.8"
+  val flow = "com.github.jodersky" %% "flow" % "2.0.9"
+  val flowNative = "com.github.jodersky" % "flow-native" % "2.0.9"
 
   val bootstrap = "org.webjars" % "bootstrap" % "3.3.1"
   val fontawesome = "org.webjars" % "font-awesome" % "4.2.0"
